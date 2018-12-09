dc960d83d68eed41d38d1d04c140b46296dc90b2
ellivromvvn <el_livro3485@yahoo.com>
2018-12-09 20:47:03 +0800

    almost done with GIS

--- spat_stat_bay.R
+++ spat_stat_bay.R
@@ -2,12 +2,23 @@ library(rgdal)
 bay <- readOGR(dsn = "C:/Users/acer/Downloads/bayombong_nuevaviz", 
                layer = "bayombong_nuevaviz")
 plot(bay, col = "lightgrey")
-Dengue<-c(6,4,9,37,35,9,0,11,5,5,35,13,3,14,10,10,10,6,21,34,6,11,32,6,3)
-bay$Dengue<-Dengue
-qtm(shp = bay, fill = "Dengue", fill.palette = "Reds")
+Dengue<-c(6,4,10,37,9,9,0,11,5,5,35,13,3,14,10,10,35,6,21,34,6,11,32,6,3)
+bay$Cases<-Dengue
+qtm(shp = bay, fill = "Cases", fill.palette = "Reds", text = "Labs",
+    text.col = "black", fill.title = "", text.size = 0.7,
+    title = "2018 Dengue Cases in Bayombong, Nueva Vizcaya")
 
-png("Dplot.png",width=5,height=5, units="in",res=2000)
-qtm(shp = bay, fill = "Dengue", fill.palette = "Reds")
+png("Dplot.png",width=7,height=7, units="in",res=2500)
+qtm(shp = bay, fill = "Dengue", fill.palette = "Reds", text = "Labs",
+    text.col = "black", text.size = 0.7)
 dev.off()
 
 mdc<-c(10,4,3,1,0,1,4,24,38,69,0,0)
+
+Labs<-c("Cabuaan","DMM","DDM","IV","DTM","SR","Ipil-Cuneg","Casat",
+        "LTN","L","BP","Magsaysay","Masoc","BW","BE","SN","DMP",
+        "LTS","S","Busilac","Paitan","VA","Buenavista","Magapuy",
+        "Bansing")
+
+bay$Labs<-Labs
+
