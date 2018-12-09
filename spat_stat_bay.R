a6d6a3d4b0f080e6629346b8f9ed519949e704fe
ellivromvvn <el_livro3485@yahoo.com>
2018-12-10 04:54:25 +0800

    almost done for ts and map

--- spat_stat_bay.R
+++ spat_stat_bay.R
@@ -1,24 +1,53 @@
+#mapping dengue cases
+
 library(rgdal)
+library(tmap)
+
+#Importing shp file
 bay <- readOGR(dsn = "C:/Users/acer/Downloads/bayombong_nuevaviz", 
                layer = "bayombong_nuevaviz")
+
+#simple plot
 plot(bay, col = "lightgrey")
+
+#adding dengue cases variable
 Dengue<-c(6,4,10,37,9,9,0,11,5,5,35,13,3,14,10,10,35,6,21,34,6,11,32,6,3)
 bay$Cases<-Dengue
-qtm(shp = bay, fill = "Cases", fill.palette = "Reds", text = "Labs",
-    text.col = "black", fill.title = "", text.size = 0.7,
-    title = "2018 Dengue Cases in Bayombong, Nueva Vizcaya")
 
-png("Dplot.png",width=7,height=7, units="in",res=2500)
-qtm(shp = bay, fill = "Dengue", fill.palette = "Reds", text = "Labs",
-    text.col = "black", text.size = 0.7)
-dev.off()
-
-mdc<-c(10,4,3,1,0,1,4,24,38,69,0,0)
-
+#adding labels variable
 Labs<-c("Cabuaan","DMM","DDM","IV","DTM","SR","Ipil-Cuneg","Casat",
         "LTN","L","BP","Magsaysay","Masoc","BW","BE","SN","DMP",
         "LTS","S","Busilac","Paitan","VA","Buenavista","Magapuy",
         "Bansing")
-
 bay$Labs<-Labs
 
+#the map
+qtm(shp = bay, fill = "Cases", fill.palette = "Reds", text = "Labs",
+    text.col = "black", fill.title = "", text.size = 0.7,
+    title = "2018 Dengue Cases in Bayombong, Nueva Vizcaya")
+
+#editing saved file
+png("Dplot.png",width=7,height=7, units="in",res=2500)
+qtm(shp = bay, fill = "Cases", fill.palette = "Reds", text = "Labs",
+    text.col = "black", fill.title = "", text.size = 0.7,
+    title = "2018 Dengue Cases in Bayombong, Nueva Vizcaya")
+dev.off()
+
+#TS of dengue cases
+
+library(tidyverse)
+library(lubridate)
+
+# the data
+mdc<-c(10,4,3,1,0,1,4,24,38,69,0,0)
+a<-make_date(year = 2018, month = 1:12)
+dengs<-data.frame(a,mdc)
+
+#plot
+b<-ggplot(dengs, aes(a, mdc)) +
+  geom_line()
+b + xlab("Months") + ylab("Number of Dengue Cases") + ggtitle("2018 Monthly Dengue Cases of Bayombong, Nueva Vizcaya")
+
+png("Dplot_ts.png",width=8.37,height=4.89, units="in",res=2500)
+b + xlab("Months") + ylab("Number of Dengue Cases") + ggtitle("2018 Monthly Dengue Cases of Bayombong, Nueva Vizcaya")
+dev.off()
