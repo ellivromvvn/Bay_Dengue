0be27d5416281d44e05b883608335328e04aea64
ellivromvvn <el_livro3485@yahoo.com>
2018-12-10 16:02:14 +0800

    finalized

--- spat_stat_bay.R
+++ spat_stat_bay.R
@@ -19,12 +19,27 @@ Labs<-c("Cabuaan","DMM","DDM","IV","DTM","SR","Ipil-Cuneg","Casat",
         "LTN","L","BP","Magsaysay","Masoc","BW","BE","SN","DMP",
         "LTS","S","Busilac","Paitan","VA","Buenavista","Magapuy",
         "Bansing")
-bay$Labs<-Labs
+bay$Labs1<-Labs1
 
+Labs1<-c("Cabuaan","Don Mariano Marcos","Don Domingo Madella",
+        "District IV","Don Tomas Madella","Santa Rosa","Ipil-Cuneg",
+        "Casat","La Torre North","Luyang","Bonfal Proper","Magsaysay",
+        "Masoc","Bonfal West","Bonfal East","San Nicolas",
+        "Don Mariano Perez","La Torre South","Salvacion","Busilac",
+        "Paitan","Vista Alegre","Buenavista","Magapuy","Bansing")
 #the map
+
+tmap_mode("view")
+
 qtm(shp = bay, fill = "Cases", fill.palette = "Reds", text = "Labs",
     text.col = "black", fill.title = "", text.size = 0.7,
     title = "2018 Dengue Cases in Bayombong, Nueva Vizcaya")
+    
+map1 <- tm_shape(bay) +
+  tm_polygons('Cases', title = "",palette = 'Reds', alpha = 0.7, id="Labs1")
+map1 + tm_layout(basemaps = c('OpenStreetMap'), title = "2018 Dengue Cases in Bayombong, Nueva Vizccaya", title.size = 10)
+
+
 
 #editing saved file
 png("Dplot.png",width=7,height=7, units="in",res=2500)
